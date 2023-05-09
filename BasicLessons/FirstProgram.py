45ee7460c34cf08aa66e16c3e35da49110a397b8
Rafael Oliva <rafa.oliva950713@hotmail.com>
2023-05-09 07:48:57 -0400

    Adding my first programm in python

---
+++ BasicLessons/FirstProgram.py
@@ -0,0 +1,22 @@
+#Here I'm placing the basic commands and variable declaration in python
+
+#My first string
+print("Hello World")
+
+#First variables
+firstName = 'Rafael'
+lastName = 'Oliva'
+age= 27     #python automatically detect the type of variable
+
+# print variables using 'f' before the text and place the variables inside keys {}
+
+print(f"Hello Mr. {lastName}")
+print(f"{firstName} has {age} years old")
+
+#Users can insert text using input
+favoriteColor= input("Please insert your favorite color: ")
+print(F"Mr. {lastName} favorite color is {favoriteColor}")
+
+#Exercise
+#Create a program to create your beer branch name
+print("Your beer name could be: "+favoriteColor+lastName)
